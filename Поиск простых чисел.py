fea0bee1b25a0a91ce330430ecbf9274d6ef6a3a
AndreiMusatov <166915210+AndreiMusatov@users.noreply.github.com>
2024-05-02 16:52:45 +0300

    Add files via upload

---
+++ Поиск простых чисел.py
@@ -0,0 +1,15 @@
+﻿n=int(input("Поиск простых чисел до "))
+a=[2]
+for i in range(3, n+1, 2):
+	k=0
+	q=int(n**0.5)
+	for j in a:
+		if j>q:
+			break
+		if i%j==0:
+			k=1
+			break
+	if k==0:
+		a.append(i)
+print("ОБЩИЕ КОЛИЧЕСТВО ЧИСЕЛ: "+ str(len(a)))
+print(a)
